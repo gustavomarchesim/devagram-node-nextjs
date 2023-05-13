09634c414e9342acea6995ff9a6465d7b7ed920e
Gustavo Marchesim <105670464+gustavomarchesim@users.noreply.github.com>
2023-05-13 13:18:02 -0300

    Models

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { respostasPadroes } from '../../types/respostasPadroes';
 import { cadastroRequisicao } from '../../types/cadastroRequisicao';
+import { usuarioModel } from '../../models/usuarioModel';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import md5 from 'md5';
 
-const endpointCadastro = ( req : NextApiRequest, res : NextApiResponse<respostasPadroes> ) => {
-
+const endpointCadastro = async ( req : NextApiRequest, res : NextApiResponse<respostasPadroes> ) => {
     if(req.method === 'POST'){
         const usuario = req.body as cadastroRequisicao;
-
+        //Faz a validação das informações passadas pelo usuário.
         if(!usuario.nome || usuario.nome.length < 5) {
             return res.status(400).json({erro : 'Nome inválido!'});
         }
@@ -18,9 +20,23 @@ const endpointCadastro = ( req : NextApiRequest, res : NextApiResponse<respostas
         if(!usuario.senha || usuario.senha.length < 4) {
             return res.status(400).json({erro : 'Senha inválido!'}); 
         }
-        return res.status(200).json({msg : 'Dados corretos!'});
+
+        //verifica se o email cadastrado já existe no banco de dados!
+        const usuariosDuplicidadeEmail = await usuarioModel.find({email : usuario.email});
+        if(usuariosDuplicidadeEmail && usuariosDuplicidadeEmail.length > 0){
+            res.status(400).json({erro : 'O email cadastrado ja existe!' });
+        };
+        //Salva informações no banco de dados
+        const usuarioFinal = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        };
+        
+        await usuarioModel.create(usuarioFinal);
+        return res.status(200).json({msg : 'Usuário cadastrado com sucesso!'});
     }
     return res.status(405).json({erro : 'Método informado não é válido!'});
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import {  respostasPadroes } from "../../types/respostasPadroes";
+import { usuarioModel } from "../../models/usuarioModel";
+import md5 from 'md5';
 
-const endpointLogin = ( req: NextApiRequest, res: NextApiResponse<respostasPadroes> ) => {
+/**
+ * This is a TypeScript function that handles a login endpoint, checking if the provided login and
+ * password match a user in the database and returning an appropriate response.
+ * 
+ * @param req The `req` parameter is an object that represents the incoming HTTP request in a Next.js
+ * API route. It contains information about the request, such as the HTTP method, headers, query
+ * parameters, and request body.
+ * @param res The "res" parameter is an instance of the NextApiResponse class, which is used to send
+ * the HTTP response back to the client. It is used to set the status code, headers, and body of the
+ * response. In this case, it is of type "NextApiResponse<respostasPad
+ * 
+ * @return The endpointLogin function is returning a response to the client based on the HTTP method
+ * used in the request and the validity of the login credentials provided in the request body. If the
+ * method is POST and the login credentials are valid, a success message with the user's name is
+ * returned with a status code of 200. If the login credentials are invalid, an error message is
+ * returned with a status code of
+ */
+const endpointLogin = async ( req: NextApiRequest, res: NextApiResponse<respostasPadroes> ) => {
     if (req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin' && senha === 'admin2123') {
-            return res.status(200).json({msg: 'Usuário autenticado com sucesso!'});
+        const usuarioEncontrado = await usuarioModel.find({email: login, senha : md5(senha)});
+        if(usuarioEncontrado && usuarioEncontrado.length > 0) {
+            const usuarioLogado = usuarioEncontrado[0];
+            return res.status(200).json({msg: `Usuário ${usuarioLogado.nome} autenticado com sucesso!`});
         } 
         return res.status(400).json({erro : 'Usuário ou senha inválida!'});
     }
